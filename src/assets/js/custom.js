0341b35599229548b992786dbc9717e583496c74
Sarathbaby29 <sarathbaby29@gmail.com>
2022-07-25 14:53:34 +0530

    Deploye website

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -18,7 +18,7 @@ export const percentage = (value, total, toFixed=0) => {
 
 export const bytesToSize = (bytes) => {
   var sizes = ["Bytes", "KB", "MB", "GB", "TB"];
-  if (bytes == 0) return "0 Byte";
+  if (bytes === 0) return "0 Byte";
   var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
   return Math.round(bytes / Math.pow(1024, i), 2) + " " + sizes[i];
 };
