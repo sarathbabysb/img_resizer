387f55db43712321b0a64a1dc1e316c58c097762
Sarathbaby29 <sarathbaby29@gmail.com>
2022-07-26 11:03:28 +0530

    Header modified

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -20,7 +20,10 @@ const Header = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
             <ul className="navbar-nav">
               <li className="nav-item">
                 <NavLink
