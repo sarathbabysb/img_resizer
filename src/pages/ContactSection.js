9113a91d33b40af640ea8ffd2d012efa44f95cae
Sarathbaby29 <sarathbaby29@gmail.com>
2022-07-26 10:13:50 +0530

    aboutus,contct page modified

--- src/pages/ContactSection.js
+++ src/pages/ContactSection.js
@@ -1,8 +1,11 @@
 const ContactSection = () => {
   return (
-    <>
-      ContactSection
-    </>
+    <div className="row">
+      <div className="col-md-6 offset-md-3 col-sm-12 offset-sm-0">
+        <h4 className="title text-center">Contact</h4>
+        ContactSection
+      </div>
+    </div>
   );
 }
 
