43d952b6376409a60187317d2b9c55b8641e4a32
Sarathbaby29 <sarathbaby29@gmail.com>
2022-07-26 10:14:57 +0530

    aboutus,contct page modified

--- src/pages/AboutSection.js
+++ src/pages/AboutSection.js
@@ -3,7 +3,9 @@ const AboutSection = () => {
     <div className="row">
       <div className="col-md-6 offset-md-3 col-sm-12 offset-sm-0">
         <h4 className="title text-center">About Us</h4>
-        AboutSection
+        <div className="row">
+          AboutSection
+        </div>
       </div>
     </div>
   );
--- src/pages/ContactSection.js
+++ src/pages/ContactSection.js
@@ -3,7 +3,7 @@ const ContactSection = () => {
     <div className="row">
       <div className="col-md-6 offset-md-3 col-sm-12 offset-sm-0">
         <h4 className="title text-center">Contact</h4>
-        ContactSection
+        <div className="row">ContactSection</div>
       </div>
     </div>
   );
